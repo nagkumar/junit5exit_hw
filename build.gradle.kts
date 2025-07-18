dc734c88a0597639246b4c063c11100cfb11ce02
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2025-07-18 16:40:21 +0530

    qeweqw

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,13 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    implementation("com.ginsberg:junit5-system-exit:2.0.2")
+    testImplementation("com.ginsberg:junit5-system-exit:2.0.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.0.0-M1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
-
     jvmArgumentProviders.add(CommandLineArgumentProvider {
 	listOf("-javaagent:${
 	    configurations.testRuntimeClasspath.get().files.find {
@@ -24,4 +24,11 @@ tasks.test {
 	    }
 	}")
     })
+}
+
+tasks.register("printTestRuntimeClasspath") {
+    doLast {
+	println("All testRuntimeClasspath files:")
+	configurations.testRuntimeClasspath.get().files.forEach { println(it) }
+    }
 }
